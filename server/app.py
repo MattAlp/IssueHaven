ef9926cb5cff93666029e51aacb5d101b1fca6b7
MattAlp <alpmatthew@gmail.com>
2018-05-27 16:48:07 -0400

    Added issues page (no pagination)
    Took 45 minutes

--- server/app.py
+++ server/app.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-
+from github_searcher.models import Base
+from flask_sqlalchemy import SQLAlchemy
+import config
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URL # TODO add proper config file
+
 Bootstrap(app)
+db = SQLAlchemy(app, metadata=Base.metadata)
+
 import server.views
--- server/views.py
+++ server/views.py
@@ -1,8 +1,15 @@
 from flask import render_template
-from .app import app
+from github_searcher.models import Issue
+from .app import app, db
 
+# TODO create actual templates instead of placeholders
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.jinja2")
+
+
+@app.route("/issues")
+def show_issues():
+    return render_template("issues.jinja2", issues=db.session.query(Issue).all())

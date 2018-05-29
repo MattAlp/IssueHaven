7f1302adc57509eb7a091fd7a37ead473b48ebdc
MattAlp <alpmatthew@gmail.com>
2018-05-28 20:59:01 -0400

    Add backrefs for repos and issues
    Took 28 minutes

--- server/app.py
+++ server/app.py
@@ -7,6 +7,7 @@ import config
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URL # TODO add proper config file
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 Bootstrap(app)
 db = SQLAlchemy(app, metadata=Base.metadata)

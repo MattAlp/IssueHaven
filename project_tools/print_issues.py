2fdf66b51c1fce29302224f32cc918ca4ee422ff
MattAlp <alpmatthew@gmail.com>
2018-05-28 21:07:39 -0400

    Add print all issues tool, should probably add tests later
    Took 8 minutes

---
+++ project_tools/print_issues.py
@@ -0,0 +1,15 @@
+from github_searcher.models import Base, engine, Issue, Repo
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    Base.metadata.create_all(engine)
+    session_factory = sessionmaker(bind=engine)
+    session = session_factory()
+
+    for repo in session.query(Repo).all():
+        print("%s - %d stars, lang:%s" % (repo.name, repo.total_stars, repo.language))
+        for issue in repo.issues:
+            print("\t[%s]\t%s with %d comments, created at %s" % (issue.category, issue.title, issue.total_comments,
+                                                                issue.created_at))
+        print()

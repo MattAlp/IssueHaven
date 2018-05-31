22488aac699b449aa466381e9722eab4755dbb83
MattAlp <alpmatthew@gmail.com>
2018-05-30 20:07:35 -0400

    Added pagination
    Took 1 hour 18 minutes

--- server/views.py
+++ server/views.py
@@ -4,12 +4,17 @@ from .app import app, db
 
 # TODO create actual templates instead of placeholders
 
+
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.jinja2")
 
 
-@app.route("/issues")
-def show_issues():
-    return render_template("issues.jinja2", issues=db.session.query(Issue).all())
+@app.route("/issues/<int:page>")
+def show_issues(page):
+
+    issues = db.session.query(Issue).paginate(page=page, per_page=15)
+
+    # return render_template("issues.jinja2", issues=db.session.query(Issue).all())
+    return render_template("issues.jinja2", issues=issues.items)

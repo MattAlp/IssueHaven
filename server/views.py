79ebda2bafc6d90d4e99505f7e8bab98a7ba7d0a
MattAlp <alpmatthew@gmail.com>
2018-06-01 20:20:09 -0400

    Added sort by issue language + simple algorithm for ranking (although it's just a SQL select right now)
    Took 1 hour 5 minutes

--- server/views.py
+++ server/views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from github_searcher.models import Issue
+from github_searcher.models import Issue, Repo
 from .app import app, db
 
 # TODO create actual templates instead of placeholders
@@ -11,10 +11,15 @@ def index():
     return render_template("index.jinja2")
 
 
+@app.route("/issues")
 @app.route("/issues/<int:page>")
-def show_issues(page):
-
-    issues = db.session.query(Issue).paginate(page=page, per_page=15)
-
-    # return render_template("issues.jinja2", issues=db.session.query(Issue).all())
-    return render_template("issues.jinja2", issues=issues)
+@app.route("/issues/<string:language>")
+@app.route("/issues/<string:language>/<int:page>")
+def show_issues(page=1, language=None):
+    if language is not None:
+        if language.lower() == "csharp":
+            language = "c#"
+        issues = db.session.query(Issue).order_by(Issue.created_at.desc(), Issue.total_comments.desc()).join(Issue.repo).order_by(Repo.total_stars.desc()).filter(Repo.language.ilike(language)).paginate(page=page, per_page=15)
+    else:
+        issues = db.session.query(Issue).paginate(page=page, per_page=15)
+    return render_template("issues.jinja2", issues=issues, language=language)

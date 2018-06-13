a0982170316ff4a48c4a89e3bd5c1b86dab6708d
MattAlp <alpmatthew@gmail.com>
2018-06-13 19:19:27 -0400

    Basic working example of compile/print_issues, but in GraphQL
    Took 24 minutes

--- github_searcher/graphql.py
+++ github_searcher/graphql.py
@@ -1,4 +1,5 @@
 import requests
+import requests_cache
 import config
 import json
 import os
@@ -7,14 +8,14 @@ from typing import List
 
 
 GRAPHQL_URL = "https://api.github.com/graphql"
-query = """
+REPO_ISSUES_QUERY = """
 {{
   repository(owner: "{owner}", name: "{name}") {{
     databaseId
     primaryLanguage {{
       name
     }}
-    issues(first: 100, labels: {labels}, after: {after}, states:OPEN){{
+    issues(first: 100, labels: {labels}, after: {endCursor}, states:OPEN){{
       nodes{{
         title
         url
@@ -29,23 +30,29 @@ query = """
 """
 
 
-def get_issues(name: str, owner: str, labels: List[str], after: str = None):
-    if after is not None:
-        after = "\"" + after + "\""
+def get_issues(name: str, owner: str, labels: List[str], endCursor: str = None):
+    if endCursor is not None:
+        endCursor = "\"" + endCursor + "\""
     else:
-        after = "null"
+        endCursor = "null"
     headers = {'Authorization': 'token %s' % config.TOKEN}
-    generated_query = query.format_map(locals()).replace("'", "\"")
-    query_results = requests.post(GRAPHQL_URL, json={"query": generated_query}, headers=headers).json()
+    generated_query = REPO_ISSUES_QUERY.format_map(locals()).replace("'", "\"")
+    query_results = requests.post(GRAPHQL_URL, json={"query": generated_query}, headers=headers).json()["data"]
     return query_results
 
 
 if __name__ == "__main__":
-    data = get_issues(name="ansible", owner="ansible", labels=["docs"])
-    for issue in data["data"]["repository"]["issues"]["nodes"]:
-        print(issue["title"])
-    print("-----------------------")
-    if data["data"]["repository"]["issues"]["pageInfo"]["hasNextPage"]:
-        data = get_issues(name="ansible", owner="ansible", after=data["data"]["repository"]["issues"]["pageInfo"]["endCursor"], labels=["docs"])
-    for issue in data["data"]["repository"]["issues"]["nodes"]:
-        print(issue["title"])
+    requests_cache.install_cache("GraphQL_Cache", backend="sqlite", expire_after=3600)
+    with open(os.path.join(config.PROJECT_ROOT, "repos.json"), "r") as f:
+        repos = json.load(f)
+        for json_repo in repos["repos"]:
+            owner, name = json_repo["name"].split("/")
+            print(name)
+            data = get_issues(name=name, owner=owner, labels=json_repo["code_labels"] + json_repo["chore_labels"])
+            while True:
+                for issue in data["repository"]["issues"]["nodes"]:
+                    print("\t" + issue["title"])
+                if not data["repository"]["issues"]["pageInfo"]["hasNextPage"]:
+                    break
+                else:
+                    data = get_issues(name=name, owner=owner, labels=json_repo["code_labels"], endCursor=data["repository"]["issues"]["pageInfo"]["endCursor"])

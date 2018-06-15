bc186d4168852a7f0ea0507608c139d9c70b48e1
MattAlp <alpmatthew@gmail.com>
2018-06-15 18:56:52 -0400

    basic objct properties added, not gonna keep it like this though
    Took 2 hours 36 minutes

--- github_searcher/graphql.py
+++ github_searcher/graphql.py
@@ -1,8 +1,6 @@
 import requests
-import requests_cache
 import config
-import json
-import os
+
 
 from typing import List
 
@@ -30,6 +28,29 @@ REPO_ISSUES_QUERY = """
 """
 
 
+class GraphQLQuery(object):
+    query_base = ""
+    json = ""
+    headers = {'Authorization': 'token %s' % config.TOKEN}
+
+    def __init__(self, **kwargs):
+        self.json = requests.post(GRAPHQL_URL, json={"query": self.query_base.format_map(kwargs).replace("'", "\"")},
+                                  headers=self.headers).json()["data"]
+
+
+class RepoIssuesQuery(GraphQLQuery):
+    query_base = REPO_ISSUES_QUERY
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.issues = type('', (), {})()  # https://stackoverflow.com/questions/2280334/shortest-way-of-creating-an-object-with-arbitrary-attributes-in-python
+        self.issues.nodes = self.json["repository"]["issues"]["nodes"]
+        self.issues.pageInfo = self.json["repository"]["issues"]["pageInfo"]
+        self.repo = type('', (), {})()
+        self.repo.id = self.json["repository"]["databaseId"]
+        self.repo.language = self.json["repository"]["primaryLanguage"]["name"]
+
+
 def get_issues(name: str, owner: str, labels: List[str], endCursor: str = None):
     if endCursor is not None:
         endCursor = "\"" + endCursor + "\""
@@ -42,17 +63,5 @@ def get_issues(name: str, owner: str, labels: List[str], endCursor: str = None):
 
 
 if __name__ == "__main__":
-    requests_cache.install_cache("GraphQL_Cache", backend="sqlite", expire_after=3600)
-    with open(os.path.join(config.PROJECT_ROOT, "repos.json"), "r") as f:
-        repos = json.load(f)
-        for json_repo in repos["repos"]:
-            owner, name = json_repo["name"].split("/")
-            print(name)
-            data = get_issues(name=name, owner=owner, labels=json_repo["code_labels"] + json_repo["chore_labels"])
-            while True:
-                for issue in data["repository"]["issues"]["nodes"]:
-                    print("\t" + issue["title"])
-                if not data["repository"]["issues"]["pageInfo"]["hasNextPage"]:
-                    break
-                else:
-                    data = get_issues(name=name, owner=owner, labels=json_repo["code_labels"], endCursor=data["repository"]["issues"]["pageInfo"]["endCursor"])
+    r = RepoIssuesQuery(name="ansible", owner="ansible", labels=["docs"], endCursor="null")
+    print(r.repo.language)

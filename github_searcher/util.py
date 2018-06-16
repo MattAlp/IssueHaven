17c613b587860f7e651ceb912b927c10c7e88b7c
MattAlp <alpmatthew@gmail.com>
2018-06-16 12:53:06 -0400

    Fixed time representation
    Took 34 seconds

--- github_searcher/util.py
+++ github_searcher/util.py
@@ -5,7 +5,7 @@ def rate_limit_github(func, client):
     def wrapped():
         print(client.rate_limiting)
         if client.rate_limiting[0] <= 5:
-            print("[INFO] Rate limit approached, sleeping until ", client.rate_limiting_resettime)
+            print("[INFO] Rate limit approached, sleeping until", time.ctime(client.rate_limiting_resettime))
             try:
                 time.sleep(client.rate_limiting_resettime - time.time())
             except:

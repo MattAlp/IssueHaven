bfa0629a3b439426db1ea765f0fefef11201b5e4
Matt <alpmatthew@gmail.com>
2018-09-10 17:28:20 -0400

    Added sys path modification to run print_issues separately
    Took 7 minutes

--- project_tools/print_issues.py
+++ project_tools/print_issues.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from github_searcher.models import Base, engine, Repo
 from sqlalchemy.orm import sessionmaker
 
